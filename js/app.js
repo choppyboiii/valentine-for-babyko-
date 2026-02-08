bc4634151890292f4030694e599772d12926cdb3
choppyboiii <lagmanpogi10@gmail.com>
2026-02-09 00:41:46 +0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -6,12 +6,11 @@ const vid = document.querySelector("video");
 let count = 2;
 
 const gifs = [
-  "../resources/cat-heart.gif",
-  "../resources/rusure.gif",
-  "../resources/3shocked-1.gif",
-  "../resources/4.crying.gif",
-  "../resources/5.crying.gif",
-  "../resources/idc.gif"
+  "resources/gulat.jpg",
+  "resources/rusure.gif",
+  "resources/4.crying.gif",
+  "resources/cute-panda.gif",
+  "resources/POGI.jpg"
 ];
 
 // to load the gifs faster
@@ -23,21 +22,21 @@ gifs.forEach(gifSrc => {
 // changes for when NO button is clicked
 no.addEventListener("click", () => {
   if (count == 2) {
-    gif.src = "../resources/gulat.jpg";// gif credit: https://knowyourmeme.com/photos/2738959-mr-fresh-side-eye-cat
+    gif.src = "resources/gulat.jpg";// gif credit: https://knowyourmeme.com/photos/2738959-mr-fresh-side-eye-cat
     text.innerHTML = "yah nag NO sya😬";
     yes.style.height = "65%";
     yes.style.width = "60%";
     no.style.width = "30%";
     count++;
   } else if (count == 3) {
-    gif.src = "../resources/rusure.gif";// gif credit: https://tenor.com/view/tkthao219-peach-goma-gif-25008901
+    gif.src = "resources/rusure.gif";// gif credit: https://tenor.com/view/tkthao219-peach-goma-gif-25008901
     text.innerHTML = "SURE NABA YAN BEBE? HMM🤨";
     yes.style.height = "70%";
     yes.style.width = "70%";
     no.style.width = "20%";
     count++;
   } else if (count == 4) {
-    gif.src = "../resources/4.crying.gif";// gif credit: https://tenor.com/view/cat-kitty-gif-25340141
+    gif.src = "resources/4.crying.gif";// gif credit: https://tenor.com/view/cat-kitty-gif-25340141
     text.innerHTML = "sige pag ako umiyak...😭";
     yes.style.height = "80%";
     yes.style.width = "80%";
@@ -45,7 +44,7 @@ no.addEventListener("click", () => {
     no.style.width = "10%";
     count++;
   } else if (count == 5) {
-    gif.src = "../resources/cute-panda.gif";// gif credit: https://tenor.com/view/cute-panda-happy-tease-oh-really-gif-16763330
+    gif.src = "resources/cute-panda.gif";// gif credit: https://tenor.com/view/cute-panda-happy-tease-oh-really-gif-16763330
     text.innerHTML = "HEHE wala kana kawala baby😘";
     yes.style.height = "90%";
     yes.style.width = "96%";
@@ -56,7 +55,7 @@ no.addEventListener("click", () => {
 // changes for when YES button is clicked
 yes.addEventListener("click", () => {
   vid.style.display = "block";
-  gif.src = "../resources/POGI.jpg";// gif credit: https://tenor.com/view/peach-cat-kiss-animated-love-mwah-gif-25743978
+  gif.src = "resources/POGI.jpg";// gif credit: https://tenor.com/view/peach-cat-kiss-animated-love-mwah-gif-25743978
   text.innerHTML = "Sabi ko na love moko eh 😘";
   yes.innerHTML = '<a href="https://www.instagram.com/gab.lagman/">Happy Valentines Bebe, iloveyou!😘 msg me na</a>';// inside the " " put your social media profile link
   yes.style.height = "90%";
